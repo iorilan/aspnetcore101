4fa7756469b66942747eb7751002bfd53c4323b3
lanliang <iorilan@hotmail.com>
2020-02-28 22:47:04 +0800

    w

---
+++ 29-Security/UrlEncryption/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleUrl = "param=324?param2=aaabb";
+            var encrypt = UrlEncryption.Encrypt(sampleUrl);
+            Console.WriteLine(encrypt);
+
+            var decrypt = UrlEncryption.Decrypt(encrypt);
+            Console.WriteLine(decrypt);
+        }
+    }
+}
